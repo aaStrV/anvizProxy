d91956ffa3660f3b662e9fb0720c9bf3b171fd40
serg <telegatrollej@yandex.ru>
2017-03-19 20:06:06 +0300

    YAML-config integrated

--- app/Main.hs
+++ app/Main.hs
@@ -5,22 +5,38 @@ import Control.Concurrent
 import Control.Concurrent.Chan
 import System.Exit
 import Data.Yaml
+import Control.Applicative
 
-import Lib(Message(..))
+import Lib  (Message(..)
+            ,Config(..)
+            ,AnvizConfig(..)
+            ,SerialConfig(..)
+            ,ServerConfig(..)
+            ,ActionsConfig(..))
 import CThread
 import MThread
 import SThread
 
 main :: IO ()
 main = do
-    checkArgs
-    [h,dp,sp,prog] <- getArgs --host, destport, srcport, prog_to_run
+    [cpath] <- getArgs --path to config file
+    c <- readMyConfig cpath
+    let
+        h = anviz_ip $ anviz c
+        dp = anviz_port $ anviz c
+        sp = server_port $ server c
+        prun = run $ actions c
+    putStrLn $ "Host: "++h
+    putStrLn $ "DPort: "++dp
+    putStrLn $ "SPort: "++sp
+    putStrLn $ "Run script: "++prun
     chan <- newChan
     _ <- forkIO $ cThread h dp chan
     _ <- forkIO $ sThread sp chan
-    mThread chan prog
+    mThread chan prun
     print "Main: something goes wrong. Bye-bye"
 
+{-
 checkArgs = do
     progName <- getProgName
     args <- getArgs
@@ -34,5 +50,11 @@ checkArgs = do
             printHelp progName
             die "Wrong number of arguments"
        else return ()
+-}
 
 printHelp pn = putStrLn $ "Usage:    "++pn++" dst_host dst_port src_port prog_to_run"
+
+readMyConfig :: String -> IO Config
+readMyConfig path =
+    either (error . show) id <$>
+    decodeFileEither path--"./src/config.yaml"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,74 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Lib
-    ( Message(..)
+    ( Message(..),
+    Config(..),
+    AnvizConfig(..),
+    SerialConfig(..),
+    ServerConfig(..),
+    ActionsConfig(..)
     ) where
 
 import Data.ByteString hiding (putStrLn)
+import Data.Yaml
+import Control.Applicative
 
 data Message = Request ByteString
              | Responce ByteString
+
+data Config = Config    { anviz :: AnvizConfig
+                        , serial :: SerialConfig
+                        , server :: ServerConfig
+                        , actions :: ActionsConfig
+                        } deriving Show
+
+instance FromJSON Config where
+    parseJSON (Object m) = Config <$>
+        m .: "anviz" <*>
+        m .: "serial" <*>
+        m .: "server" <*>
+        m .: "actions"
+    parseJSON x = fail ("not an object: " ++ show x) 
+
+data AnvizConfig = AnvizConfig {
+    anviz_ip :: String,
+    anviz_port :: String,
+    anviz_users :: [Int]
+} deriving Show
+
+instance FromJSON AnvizConfig where
+    parseJSON (Object m) = AnvizConfig <$>
+        m .: "anviz_ip" <*>
+        m .: "anviz_port" <*>
+        m .: "anviz_users"
+    parseJSON x = fail ("not an object: " ++ show x) 
+
+data SerialConfig = SerialConfig {
+    serial_port :: String,
+    serial_users :: [Int]
+} deriving Show
+
+instance FromJSON SerialConfig where
+    parseJSON (Object m) = SerialConfig <$>
+        m .: "serial_port" <*>
+        m .: "serial_users"
+    parseJSON x = fail ("not an object: " ++ show x) 
+
+data ServerConfig = ServerConfig {
+    server_eth :: String,
+    server_port :: String
+} deriving Show
+
+instance FromJSON ServerConfig where
+    parseJSON (Object m) = ServerConfig <$>
+        m .: "server_eth" <*>
+        m .: "server_port"
+    parseJSON x = fail ("not an object: " ++ show x) 
+
+data ActionsConfig = ActionsConfig {
+    run :: String
+} deriving Show
+
+instance FromJSON ActionsConfig where
+    parseJSON (Object m) = ActionsConfig <$>
+        m .: "run"
+    parseJSON x = fail ("not an object: " ++ show x) 
