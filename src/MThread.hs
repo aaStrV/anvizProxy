184a8580dac9f3eed8093e38d8c987ed3943a1cf
serg <telegatrollej@yandex.ru>
2017-03-19 16:11:40 +0300

    Alpha version.
    TODO:
    - configuration file parsing
    - test presence of target script on start

--- src/MThread.hs
+++ src/MThread.hs
@@ -3,8 +3,9 @@ module MThread --middle part, analizing messages and traffic
 ) where
 
 import Control.Concurrent.Chan
-import Data.ByteString hiding (putStrLn, putStr)
+import Data.ByteString as B hiding (putStrLn, putStr)
 import Control.Monad
+import System.Process
 
 import Lib(Message(..))
 
@@ -18,11 +19,24 @@ mBody chan p = do
         msg <- readChan chan
         case msg of
              Request message     -> do
-                 putStr "Middle(mBoby): Request "
-                 print message
+                 --putStr "Middle(mBoby): Request "
+                 return ()
              Responce message    -> do
-                 putStr "Middle(mBoby): Responce "
-                 print message
-             --_                          -> print "Unknown message receaved"
+                 --putStr "Middle(mBoby): Responce "
+                 --print message
+                 let m = B.unpack message
+                 --print m
+                 if analizeResp m
+                    then do
+                        putStrLn "Middle(mBoby): Alarm!"
+                        _ <- createProcess (proc "ls" [])
+                        return ()
+                    else return ()
 
-analizeReq m = m
+--analizeResp :: [Word8] -> Bool
+analizeResp [] = False
+--analizeResp (x:xs) = if x == 0xa5 then True else False
+analizeResp (0xa5:_:_:_:_:0xdf:0x00:0x00:0x0e:0x00:0x00:0x00:0x00:0x08:xs) = True
+analizeResp _ = False
+
+--checkResp :: [a] -> [a]
